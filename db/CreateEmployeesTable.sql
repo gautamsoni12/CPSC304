a79a59833ca7b972e7460c1210bd9dca1de0b72d
Azlan Mustapha <ajmustapha@gmail.com>
2018-03-23 15:04:32 -0700

    fixed bad syntax

--- db/CreateEmployeesTable.sql
+++ db/CreateEmployeesTable.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXSISTS Employees
+DROP TABLE IF EXISTS Employees;
 CREATE TABLE Employees(
   employee_id SERIAL,
   fname VARCHAR(50) NOT NULL,
@@ -8,17 +8,17 @@ CREATE TABLE Employees(
   employee_sin VARCHAR(9) UNIQUE NOT NULL,
   email VARCHAR(320) UNIQUE NOT NULL,
   warehouse_id INTEGER,
-  address VARCHAR(32),
+  address VARCHAR(50),
   position VARCHAR(50),
   PRIMARY KEY (employee_id),
   FOREIGN KEY (warehouse_id) REFERENCES Warehouse(warehouse_id)
 );
 
 INSERT INTO Employees (FNAME, LNAME, PASSWORD, PHONE, EMPLOYEE_SIN, EMAIL, WAREHOUSE_ID, ADDRESS, POSITION)
-  VALUES (‘Penny’, 'Peaches', ‘happy’, '613-555-0139', 123456789, ‘p@abc.com’, 5, ‘12 Rand Ave.’, ‘Senior Manager’);
+VALUES (‘Penny’, 'Peaches', ‘happy’, '613-555-0139', 123456789, ‘p@abc.com’, 5, ‘12 Rand Ave.’, ‘Senior Manager’);
 
 INSERT INTO Employees (FNAME, LNAME, PASSWORD, PHONE, EMPLOYEE_SIN, EMAIL, WAREHOUSE_ID, ADDRESS, POSITION)
-  VALUES (‘Issey’, 'Miyaki', ‘sad’, '780-555-0159', 987364990, ‘i@bob.com’, 2, ‘245 Nowhere Street’, ‘Cashier’);
+VALUES (‘Issey’, 'Miyaki', ‘sad’, '780-555-0159', 987364990, ‘i@bob.com’, 2, ‘245 Nowhere Street’, ‘Cashier’);
 
 INSERT INTO Employees (FNAME, LNAME, PASSWORD, PHONE, EMPLOYEE_SIN, EMAIL, WAREHOUSE_ID, ADDRESS, POSITION)
-  VALUES (‘Matt’, 'LeBlanc', ‘sun’, '418-555-0169', 937463745, ‘m@tim.com’, ‘5’, ‘152 Penny Lane’, ‘Manager’);
+VALUES (‘Matt’, 'LeBlanc', ‘sun’, '418-555-0169', 937463745, ‘m@tim.com’, ‘5’, ‘152 Penny Lane’, ‘Manager’);
